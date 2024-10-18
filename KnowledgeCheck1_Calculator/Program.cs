2fccae70d1f63a99a1c80c4ae4e58503d6b24a51
Mark W. Kidd <mark@stardart.net>
2024-10-18 17:40:24 -0400

    add function and complete assignment

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -15,47 +15,40 @@ namespace KnowledgeCheck1_Calculator
 
             var input = Console.ReadLine();
             var calculator = new Calculator();
+            int inNumber1 = 0;
+            int inNumber2 = 0;
 
             switch (input)
             {
                 case "1":
                     Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
-
-                    if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
+                    if (ReadIntegers(out inNumber1, out inNumber2))
                     {
-                        Console.Write($"{addNumber1} + {addNumber2} = ");
-                        Console.Write(calculator.Add(addNumOne, addNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
+                        Console.Write($"{inNumber1} + {inNumber2} = ");
+                        Console.Write(calculator.Add(inNumber1, inNumber2));
                     }
                     break;
 
                 case "2":
                     Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
-
-                    if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
+                    if (ReadIntegers(out inNumber1, out inNumber2))
                     {
-                        Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
-                        Console.Write(calculator.Subtract(subNumOne, subNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
+                        Console.Write($"{inNumber1} - {inNumber2} = ");
+                        Console.Write(calculator.Subtract(inNumber1, inNumber2));
                     }
                     break;
 
                 case "3":
-                    // Add code here
+                    Console.WriteLine("Enter 2 integers to multiply");
+                    if (ReadIntegers(out inNumber1, out inNumber2))
+                    {
+                        Console.Write($"{inNumber1} - {inNumber2} = ");
+                        Console.Write(calculator.Multiply(inNumber1, inNumber2));
+                    }
                     break;
 
                 case "4":
-                    Console.WriteLine("Enter 2 integers to divide");
+                    Console.WriteLine("Enter 2 integers to divide"); // the division logic uses doubles to store the numbers. should we tell the user?
                     var divideNumber1 = Console.ReadLine();
                     var divideNumber2 = Console.ReadLine();
 
@@ -66,14 +59,29 @@ namespace KnowledgeCheck1_Calculator
                     }
                     else
                     {
-                        Console.WriteLine("One or more of the numbers is not an int");
+                        Console.WriteLine("One or more of the numbers is not an int"); // should this be corrected to say double?
                     }
-                    break;
                     break;
 
                 default:
                     Console.WriteLine("Unknown input");
                     break;
+            }
+        }
+
+        private static bool ReadIntegers(out int in1, out int in2)
+        {
+            in1 = 0;
+            in2 = 0;
+
+            if (!int.TryParse(Console.ReadLine().Trim(), out in1) || !int.TryParse(Console.ReadLine().Trim(), out in2))
+            {
+                Console.WriteLine("One or more of the numbers is not an int");
+                return false; // error state
+            }
+            else
+            {
+                return true;
             }
         }
     }
